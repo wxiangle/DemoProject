2af467b4877b328990522394799586a5cd8b3a33
wangxiangle <wangxiangle@52toys.com>
2022-11-03 18:26:48 +0800

    add xml file

---
+++ MvvpDemo/app/src/main/java/org/aaron/mvvpdemo/bean/Person.kt
@@ -0,0 +1,7 @@
+package org.aaron.mvvpdemo.bean
+
+/**
+ * Created by wangxl1 on 2022/11/3 15:58
+ * E-Mail Address： wang_x_le@163.com
+ */
+data class Person(val age: Int, val name: String)
---
+++ MvvpDemo/app/src/main/java/org/aaron/mvvpdemo/bean/UserInfo.kt
@@ -0,0 +1,12 @@
+package org.aaron.mvvpdemo.bean
+
+import androidx.databinding.ObservableField
+
+/**
+ * Created by wangxl1 on 2022/11/3 16:12
+ * E-Mail Address： wang_x_le@163.com
+ */
+class UserInfo() {
+    public var name: ObservableField<String> = ObservableField()
+    public var pwd: ObservableField<String> = ObservableField()
+}
